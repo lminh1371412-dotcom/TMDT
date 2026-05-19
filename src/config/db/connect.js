0b5c5dc8da92b6847f4ec1a04ac1bc33e39a83f5
Lê Văn Minh <MinhLV.B22CN534@stu.ptit.edu.vn>
2026-05-19 21:02:06 +0700

    fix1

--- src/config/db/connect.js
+++ src/config/db/connect.js
@@ -1,22 +1,20 @@
 const mysql = require('mysql');
-const express = require('express');
-const router = express.Router();
-const bodyParser = require('body-parser');
-const dotenv = require("dotenv").config();
+const dotenv = require('dotenv').config();
 
 const db = mysql.createConnection({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE
-})
+    database: process.env.DATABASE,
+    port: process.env.DATABASE_PORT
+});
 
-db.connect(function (err) {
+db.connect((err) => {
     if (err) {
-        throw err;
+        console.log('Database connection failed:', err);
     } else {
-        console.log('You are already connected to the database')
+        console.log('Connected database');
     }
-})
+});
 
 module.exports = db;
